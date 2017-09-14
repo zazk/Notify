e899c4a68e282815f9a4676c23ae4c5793c8304b
zazk <juandedioz@gmail.com>
2017-09-14 11:15:41 -0500

    Add Dependencies

---
+++ Notify/Notify/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Notify
+//
+//  Created by user on 9/14/17.
+//  Copyright © 2017 cubix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Notify/Notify/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Notify
+//
+//  Created by user on 9/14/17.
+//  Copyright © 2017 cubix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
